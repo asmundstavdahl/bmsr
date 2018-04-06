0221b1a3c0af2b65ceec82e34410f422e0839457
Åsmund Stavdahl <smund3@gmail.com>
2018-04-06 10:31:33 +0200

    tabs→spaces

--- assets/js/quick-select.js
+++ assets/js/quick-select.js
@@ -2,42 +2,42 @@
 let quickSelectElement = document.getElementById("quickSelect")
 
 function findQuickSelectTargetElement() {
-	return document.querySelector(`[data-quick-select='${quickSelect.innerHTML}']`)
+    return document.querySelector(`[data-quick-select='${quickSelect.innerHTML}']`)
 }
 
 let quickSelectCancel = e => {
-	quickSelectElement.innerHTML = ""
+    quickSelectElement.innerHTML = ""
 }
 
 hotkey(/^\d$/, e => {
-	quickSelectElement.innerHTML += e.key
-	if(!window.hotkeyCancelQueue.includes(quickSelectCancel)){
-		window.hotkeyCancelQueue.push(quickSelectCancel)
-	}
-	
-	let highlightedElements = document.getElementsByClassName("quick-select-highlight")
-	for(let i = 0; i < highlightedElements.length; i++){
-		highlightedElements[i].classList.remove("quick-select-highlight")
-	}
-	
-	let el = findQuickSelectTargetElement()
-	if(el) {
-		el.classList.add("quick-select-highlight")
-	}
+    quickSelectElement.innerHTML += e.key
+    if(!window.hotkeyCancelQueue.includes(quickSelectCancel)){
+        window.hotkeyCancelQueue.push(quickSelectCancel)
+    }
+    
+    let highlightedElements = document.getElementsByClassName("quick-select-highlight")
+    for(let i = 0; i < highlightedElements.length; i++){
+        highlightedElements[i].classList.remove("quick-select-highlight")
+    }
+    
+    let el = findQuickSelectTargetElement()
+    if(el) {
+        el.classList.add("quick-select-highlight")
+    }
 })
 
 hotkey("Enter", e => {
-	let el = findQuickSelectTargetElement()
-	if(el) {
-		let child = el.querySelector("textarea, input, button")
-		if(child){
-			el = child
-		}
-		
-		if(el.select) el.select()
-		if(el.focus) el.focus()
-		if(el.click) el.click()
-	} else {
-		quickSelectElement.innerHTML = ""
-	}
+    let el = findQuickSelectTargetElement()
+    if(el) {
+        let child = el.querySelector("textarea, input, button")
+        if(child){
+            el = child
+        }
+        
+        if(el.select) el.select()
+        if(el.focus) el.focus()
+        if(el.click) el.click()
+    } else {
+        quickSelectElement.innerHTML = ""
+    }
 })
--- assets/js/shortcuts.js
+++ assets/js/shortcuts.js
@@ -3,26 +3,26 @@
  */
 
 document.body.addEventListener("keypress", function(e) {
-	console.log("key:" + e.key, e)
-	
-	let hotkeyId = e.key
-	
-	if (e.altKey && e.key != "Alt"){
-		hotkeyId = "Alt+" + hotkeyId
-	}
-	if (e.ctrlKey && e.key != "Control"){
-		hotkeyId = "Ctrl+" + hotkeyId
-	}
-	if (e.shiftKey && e.key != "Shift"){
-		hotkeyId = "Shift+" + hotkeyId
-	}
+    console.log("key:" + e.key, e)
+    
+    let hotkeyId = e.key
+    
+    if (e.altKey && e.key != "Alt"){
+        hotkeyId = "Alt+" + hotkeyId
+    }
+    if (e.ctrlKey && e.key != "Control"){
+        hotkeyId = "Ctrl+" + hotkeyId
+    }
+    if (e.shiftKey && e.key != "Shift"){
+        hotkeyId = "Shift+" + hotkeyId
+    }
 
-	hotkeyId = "hotkey_" + hotkeyId.toLowerCase()
+    hotkeyId = "hotkey_" + hotkeyId.toLowerCase()
 
-	console.log(window[hotkeyId])
-	if(typeof window[hotkeyId] === "function"){
-		window[hotkeyId](e)
-	} else {
-		console.warn("No callable hotkey registered for " + hotkeyId)
-	}
+    console.log(window[hotkeyId])
+    if(typeof window[hotkeyId] === "function"){
+        window[hotkeyId](e)
+    } else {
+        console.warn("No callable hotkey registered for " + hotkeyId)
+    }
 })
